3243789c92f1e6f04d7531ea22db2091e65ad50f
Wen Xin Khor <khorwenxin@gmail.com>
2021-03-22 21:21:13 +0800

    Designing Front-end

---
+++ src/java/da/CustomerDA.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package da;
+
+import domain.Customer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class CustomerDA {
+
+    private String host = "jdbc:derby://localhost:1527/guidb";
+    private String user = "guidb";
+    private String password = "guidb";
+    private String tableName = "CUSTOMER";
+    private Connection conn;
+    private PreparedStatement stmt;
+
+    public CustomerDA() {
+        createConnection();
+    }
+
+    public Customer getRecord(int CustID) {
+        createConnection();
+        String queryStr = "SELECT * FROM " + tableName + " WHERE Code = ?";
+        Customer customer = null;
+        try {
+            stmt = conn.prepareStatement(queryStr);
+            stmt.setInt(1, CustID);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+
+                customer = new Customer(CustID, rs.getString("firstName"), rs.getString("lastName"), rs.getInt("age"), rs.getString("email"), rs.getString("password"), rs.getString("gender"), rs.getString("address"), rs.getString("phoneNo"), rs.getInt("rewardPoint"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            shutDown();
+        }
+        return customer;
+    }
+
+    public void updateRecord(Customer customer) {
+        // to be implemented
+    }
+
+    public void addRecord(Customer customer) {
+        // to be implemented
+    }
+
+    public void deleteRecord(String email) {
+        // to be implemented
+    }
+
+    private void createConnection() {
+        try {
+            conn = DriverManager.getConnection(host, user, password);
+            System.out.println("***TRACE: Connection established.");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void shutDown() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+}
---
+++ src/java/domain/Customer.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Forge-15
+ */
+public class Customer implements Serializable{
+    private int custID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String password;
+    private String gender;
+    private String address;
+    private String phoneNo;
+    private int rewardPoint;
+
+
+
+    public Customer(int custID, String firstName, String lastName, int age, String email, String password, String gender, String address, String phoneNo, int rewardPoint) {
+        this.custID = custID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNo = phoneNo;
+        this.rewardPoint = rewardPoint;
+    }
+
+    
+    
+    public Customer() {
+    }
+
+    public int getCustID() {
+        return custID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setCustID(int custID) {
+        this.custID = custID;
+    }
+
+    public int getRewardPoint() {
+        return rewardPoint;
+    }
+
+    public void setRewardPoint(int rewardPoint) {
+        this.rewardPoint = rewardPoint;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "custID=" + custID + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", email=" + email + ", password=" + password + ", gender=" + gender + ", address=" + address + ", phoneNo=" + phoneNo + '}';
+    }
+
+    
+        
+    
+    
+    
+    
+}
